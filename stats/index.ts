ef44fa0f9c7f84f317c2ce76efbdd73b2d5cbea9
Andrew Baillie <andrewbaillie@me.com>
2023-08-12 19:12:09 +0100

    feat: adds simple file type list

--- stats/index.ts
+++ stats/index.ts
@@ -1,12 +1,24 @@
 import { graphql } from "@octokit/graphql";
 import { Commit, RepositoryNode } from "@octokit/graphql-schema";
 import repoList from "./repoList.json";
+import { Octokit } from "@octokit/core";
 
 const graphqlWithAuth = graphql.defaults({
   headers: {
     authorization: `Bearer ${process.env.TOKEN}`,
   },
 });
+
+const octokit = new Octokit({
+  auth: process.env.TOKEN,
+});
+
+const extensionRE = /(?:\.([^.]+))?$/;
+
+const extensionList = new Map([
+  ["ts", "TypeScript"],
+  ["json", "JSON"],
+]);
 
 interface Langauges {
   [key: string]: number;
@@ -16,6 +28,7 @@ interface RepoStats {
   totalCommits: number;
   additions: number;
   deletions: number;
+  commitIds: string[];
   languages: Langauges;
 }
 
@@ -23,7 +36,39 @@ const repoStats: RepoStats = {
   totalCommits: 0,
   additions: 0,
   deletions: 0,
+  commitIds: [],
   languages: {},
+};
+
+const getFilesInCommit = async (owner: string, repo: string, ref: string) => {
+  const commitInfo = await octokit.request(
+    "GET /repos/{owner}/{repo}/commits/{ref}",
+    {
+      owner,
+      repo,
+      ref,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    }
+  );
+
+  if (commitInfo.status === 200) {
+    return commitInfo.data.files
+      ?.map((file) => {
+        const extract = extensionRE.exec(file?.filename);
+        const extension = extract?.length ? extract[1] : null;
+
+        if (extension && extensionList.get(extension)) {
+          return extensionList.get(extension);
+        }
+
+        return extension;
+      })
+      .filter(Boolean);
+  }
+
+  return [];
 };
 
 (async () => {
@@ -46,6 +91,7 @@ const repoStats: RepoStats = {
                   endCursor
                 }
                 nodes {
+                  oid
                   additions
                   deletions
                 }
@@ -73,8 +119,24 @@ const repoStats: RepoStats = {
       repoStats.totalCommits++;
       repoStats.additions += node.additions;
       repoStats.deletions += node.deletions;
+      repoStats.commitIds.push(node.oid);
     });
 
-    console.debug(repoStats);
+    if (repoStats.commitIds) {
+      const fileList = await Promise.all(
+        repoStats.commitIds.map(async (commit) => {
+          return await getFilesInCommit(owner, name, commit);
+        })
+      );
+
+      const flatFileList = fileList.flat();
+
+      for (let i = 0; i < flatFileList.length; i++) {
+        repoStats.languages[flatFileList[i] as string] =
+          repoStats.languages[flatFileList[i] as string] + 1 || 1;
+      }
+    }
   }
+
+  console.log(repoStats);
 })();
