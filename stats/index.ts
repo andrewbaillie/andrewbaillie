dac8a02496ba68f7b0b80af8080583a8d77ace39
Andrew Baillie <andrewbaillie@me.com>
2023-08-12 18:29:42 +0100

    feat: adds simple graphql query for stats over last month

--- stats/index.ts
+++ stats/index.ts
@@ -1,3 +1,80 @@
+import { graphql } from "@octokit/graphql";
+import { Commit, RepositoryNode } from "@octokit/graphql-schema";
+import repoList from "./repoList.json";
+
+const graphqlWithAuth = graphql.defaults({
+  headers: {
+    authorization: `Bearer ${process.env.TOKEN}`,
+  },
+});
+
+interface Langauges {
+  [key: string]: number;
+}
+
+interface RepoStats {
+  totalCommits: number;
+  additions: number;
+  deletions: number;
+  languages: Langauges;
+}
+
+const repoStats: RepoStats = {
+  totalCommits: 0,
+  additions: 0,
+  deletions: 0,
+  languages: {},
+};
+
 (async () => {
-  console.log('Hello');
+  const date = new Date();
+  date.setMonth(date.getMonth() - 1);
+
+  const { owner, name, branch } = repoList[0];
+
+  const result: RepositoryNode = await graphqlWithAuth(
+    `
+    query repoStats($name: String!, $owner: String!, $branch: String!, $since: GitTimestamp!, $authorId: ID!) {
+      repository(name: $name, owner: $owner) {
+        ref(qualifiedName: $branch) {
+          target {
+            ... on Commit {
+              history(first: 100, since: $since, author: {id: $authorId}) {
+                totalCount
+                pageInfo {
+                  hasNextPage
+                  endCursor
+                }
+                nodes {
+                  additions
+                  deletions
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `,
+    {
+      owner,
+      branch,
+      name,
+      since: date.toISOString(),
+      authorId: process.env.AUTHORID,
+    }
+  );
+
+  if (result) {
+    const target = result.repository.ref?.target as Commit;
+    const nodes = target.history.nodes as Commit[];
+
+    nodes.forEach((node) => {
+      repoStats.totalCommits++;
+      repoStats.additions += node.additions;
+      repoStats.deletions += node.deletions;
+    });
+
+    console.debug(repoStats);
+  }
 })();
