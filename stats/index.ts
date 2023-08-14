5b44aaec520f14c7d719689fa86b6948ac7748f9
Andrew Baillie <andrew.baillie01@bbc.co.uk>
2023-08-14 13:23:41 +0100

    chore: change repoList to env variable

--- stats/index.ts
+++ stats/index.ts
@@ -3,62 +3,64 @@ import { getRepoInfo } from "./lib/graphql";
 import { getFilesInCommit } from "./lib/api";
 import { buildSVG } from "./lib/svg";
 
-import repoList from "./repoList.json";
 import { RepoStats } from "./types";
 
 const repoStats: RepoStats = {
-  totalRepos: 0,
-  totalCommits: 0,
-  additions: 0,
-  deletions: 0,
-  totalFiles: 0,
-  languages: {},
+    totalRepos: 0,
+    totalCommits: 0,
+    additions: 0,
+    deletions: 0,
+    totalFiles: 0,
+    languages: {},
 };
 
+const repoList = JSON.parse(process.env.REPOLIST as string);
+
 (async () => {
-  await Promise.all(
-    repoList.map(async (repo) => {
-      const { owner, name, branch } = repo;
-      const commitIds: string[] = [];
+    await Promise.all(
+        repoList.map(async (repo) => {
+            const { owner, name, branch } = repo;
+            const commitIds: string[] = [];
 
-      // Get commits from last month
-      const result = await getRepoInfo(owner, branch, name);
+            // Get commits from last month
+            const result = await getRepoInfo(owner, branch, name);
 
-      if (result) {
-        repoStats.totalRepos++;
-        const target = result.repository.ref?.target as Commit;
-        const nodes = target.history.nodes as Commit[];
+            if (result) {
+                repoStats.totalRepos++;
+                const target = result.repository.ref?.target as Commit;
+                const nodes = target.history.nodes as Commit[];
 
-        nodes.forEach((node) => {
-          repoStats.totalCommits++;
-          repoStats.additions += node.additions;
-          repoStats.deletions += node.deletions;
-          commitIds.push(node.oid);
-        });
+                nodes.forEach((node) => {
+                    repoStats.totalCommits++;
+                    repoStats.additions += node.additions;
+                    repoStats.deletions += node.deletions;
+                    commitIds.push(node.oid);
+                });
 
-        // Use commit ids to retrieve file extensions and map to type
-        if (commitIds) {
-          const fileList = await Promise.all(
-            commitIds.map(async (commit) => {
-              return await getFilesInCommit(owner, name, commit);
-            })
-          );
+                // Use commit ids to retrieve file extensions and map to type
+                if (commitIds) {
+                    const fileList = await Promise.all(
+                        commitIds.map(async (commit) => {
+                            return await getFilesInCommit(owner, name, commit);
+                        })
+                    );
 
-          const flatFileList = fileList.flat();
+                    const flatFileList = fileList.flat();
 
-          for (let i = 0; i < flatFileList.length; i++) {
-            repoStats.totalFiles++;
-            repoStats.languages[flatFileList[i] as string] =
-              repoStats.languages[flatFileList[i] as string] + 1 || 1;
-          }
-        }
-      }
-    })
-  );
+                    for (let i = 0; i < flatFileList.length; i++) {
+                        repoStats.totalFiles++;
+                        repoStats.languages[flatFileList[i] as string] =
+                            repoStats.languages[flatFileList[i] as string] +
+                                1 || 1;
+                    }
+                }
+            }
+        })
+    );
 
-  console.log(repoStats);
+    console.log(repoStats);
 
-  buildSVG(repoStats);
+    buildSVG(repoStats);
 
-  console.log("Done");
+    console.log("Done");
 })();
